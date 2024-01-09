64bc6e21e629084991e81601e2e7ae20eeddeb52
ImagineBrkr <salvattore_25@hotmail.com>
2024-01-09 17:18:26 -0500

    update script

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 pip install -r requirements.txt
 LOG_FILE="./logs"
-APP_CMD="nice -n-20 python3 main.py >> $LOG_FILE 2>&1 &"
+APP_CMD="nice -n-20 /usr/bin/python3 main.py >> $LOG_FILE 2>&1 &"
+pkill 'python3'
 start_app() {
     eval $APP_CMD
     echo "App started"
